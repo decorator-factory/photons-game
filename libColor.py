76dfc25d4743561486be40fd3b5cb519c564147a
decorator-factory <municipalomnibenevolence@yandex.ru>
2019-07-14 00:17:33 +0300

    Revert "Upload the files"
    This reverts commit fe43bf276d921c7a4eb4697eb5f756ba063be396.

--- libColor.py
+++
@@ -1,9 +0,0 @@
-from color_blockDecoupler import main as BlockDecoupler
-from color_blockNegator import main as BlockNegator
-from color_blockRemapper import main as BlockRemapper
-from color_blockCoupler import main as BlockCoupler
-from color_blockComparator import main as BlockComparator
-
-libData = {"name":"Color","desc":"Color library for Photons. Provides some blocks that perform operations on beams' color.","author":"int6h","version":"1.0.0"}
-
-__include__ = [BlockDecoupler, BlockNegator, BlockRemapper, BlockCoupler, BlockComparator]
